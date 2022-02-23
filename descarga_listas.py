dfe219a49f1f2c5cb9041828654b1ca457860c85
daedae2001 <daedae2001@mail.com>
2022-02-23 15:25:39 +0100

    a

---
+++ descarga_listas.py
@@ -0,0 +1,20 @@
+
+import requests
+mylist = [["es.m3u8", 'https://i.mjh.nz/PlutoTV/es.m3u8', 'es'],
+          ["mx.m3u8", 'https://i.mjh.nz/PlutoTV/mx.m3u8', 'mx']]
+i = 1
+primera = ""
+otras = ""
+
+for x in mylist:
+    url = x[1]
+    myfile = requests.get(url)
+    open(x[0], 'wb').write(myfile.content)
+    f = open(x[0], 'r')
+    primera = f.read()
+    f.close()
+    print(primera.find('\nhttp://'))
+    primera = primera.replace('\nhttp://', '_'+x[2]+'\nhttp://')
+    f = open(x[0], 'w')
+    f.write(primera)
+    f.close()
--- modificaxml.py
+++ modificaxml.py
@@ -1,9 +1,33 @@
 
 import requests
-mylist = [["es.xml", 'https://i.mjh.nz/PlutoTV/es.xml'],
-          ["mx.xml", 'https://i.mjh.nz/PlutoTV/mx.xml']]
+mylist = [["es.xml", 'https://i.mjh.nz/PlutoTV/es.xml', 'es'],
+          ["mx.xml", 'https://i.mjh.nz/PlutoTV/mx.xml', 'mx']]
+i = 1
+primera = ""
+otras = ""
 
 for x in mylist:
     url = x[1]
     myfile = requests.get(url)
     open(x[0], 'wb').write(myfile.content)
+    f = open(x[0], 'r')
+
+    if i == 1:
+        primera = f.read()
+        f.close()
+        primera = primera.replace('<channel id="', '<channel id="' +
+                                  x[2]).replace('<programme channel="', '<programme channel="'+x[2]).replace("<display-name>", "<display-name>"+x[2])
+    else:
+        otras = f.read()
+        f.close()
+
+        otras = otras.replace('<channel id="', '<channel id="' +
+                              x[2]).replace('<programme channel="', '<programme channel="'+x[2]).replace("<display-name>", "<display-name>"+x[2])
+        p1 = otras[0: otras.find('<programme channel="')]
+        p2 = otras[otras.find('<programme channel="')+10:len(otras)]
+        primera = primera[otras.find('<channel id="'):len(otras)]
+        primera = (p1 + primera.replace("</tv>", "") + p2)
+    i = i+1
+f = open('todo.xml', 'w')
+f.write(primera)
+f.close()
