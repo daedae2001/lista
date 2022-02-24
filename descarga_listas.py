d4051789a678fbca4d4a966fc375e2fcd145b4e0
daedae2001 <daedae2001@mail.com>
2022-02-24 13:57:26 +0100

    h

--- descarga_listas.py
+++ descarga_listas.py
@@ -14,7 +14,7 @@ for x in mylist:
     primera = f.read()
     f.close()
     print(primera.find('\nhttp://'))
-    primera = primera.replace('\nhttp://', '_'+x[2]+'\nhttp://')
+    #primera = primera.replace('\nhttp://', '_'+x[2]+'\nhttp://')
     # .replace('tvg-id="', 'tvg-id="'+x[2]).replace('-id="pluto-', "_"+x[2]+'-id="pluto-')
     f = open(x[0], 'w')
     f.write(primera)
