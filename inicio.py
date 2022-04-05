01fc99cb67fb39cf51156d94f1962dc998d2ddb5
daedae2001 <daedae2001@gmail.com>
2022-04-05 10:35:34 +0200

    a

--- inicio.py
+++ inicio.py
@@ -135,7 +135,7 @@ f.close()
 f = open('todon1.xml', 'w')
 f.write(lin+linp+'</tv>')
 f.close()
-#remove("todo.xml")
+remove("todo.xml")
 
 #df_channels = pd.DataFrame(rows_channels, columns=cols_channels)
 #df_programmes = pd.DataFrame(rows_programmes, columns=cols_programmes)
